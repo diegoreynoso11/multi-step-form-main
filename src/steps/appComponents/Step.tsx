62b3d9241996cebcf286fa875e92eed99de5fc43
apuu03 <57383340+apuu03@users.noreply.github.com>
2024-07-10 18:04:01 -0300

    agregada la validacion para poder pasar entre los steps con un click

--- src/steps/appComponents/Step.tsx
+++ src/steps/appComponents/Step.tsx
@@ -1,12 +1,18 @@
 import { useContext } from "react"
 import { appContext } from "../../context/AppContext"
 
-function Step({children, step} : {children: React.ReactNode, step: number}) {
+function Step({children, step, validation} : {children: React.ReactNode, step: number, validation? : string}) {
   const { steps, setSteps } = useContext(appContext)
-
+  function handleChangeSteps() {
+    if (validation) {
+      setSteps(step)
+    } else {
+      return
+    }
+  }
   return (
     <div className="flex gap-3">
-    <span onClick={() => setSteps(step)} className={`${steps === step ? "bg-white/50 text-blue-950" : "border-2 text-white/70"} flex items-center justify-center size-10 font-bold rounded-full cursor-pointer`}>{step}</span>
+    <span onClick={handleChangeSteps} className={`${steps === step ? "bg-white/50 text-blue-950" : "border-2 text-white/70"} flex items-center justify-center size-10 font-bold rounded-full cursor-pointer transition-all ${!validation && "opacity-30 scale-90 cursor-default"}`}>{step}</span>
     <div>
     <h2 className="text-white/50">STEP {step}</h2>
     <h1>{children}</h1>
