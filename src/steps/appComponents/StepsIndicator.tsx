23fd637e253cf13b22451b28e6a8cbedd75119a9
apuu03 <57383340+apuu03@users.noreply.github.com>
2024-07-10 18:04:27 -0300

    feat: Agregar validación para navegar entre los pasos con un clic

--- src/steps/appComponents/StepsIndicator.tsx
+++ src/steps/appComponents/StepsIndicator.tsx
@@ -1,12 +1,15 @@
+import { useContext } from "react"
 import Step from "./Step"
+import { appContext } from "../../context/AppContext"
 
 function StepsIndicator() {
-  return (
+  const { data } = useContext(appContext)
+  return ( 
     <section className="absolute flex flex-col gap-10 top-12 left-24 text-white">
-        <Step step={1}>YOUR INFO</Step>
-        <Step step={2}>SELECT PLAN</Step>
-        <Step step={3}>ADD-ONS</Step>
-        <Step step={4}>SUMMARY</Step>
+        <Step validation="true" step={1}>YOUR INFO</Step>
+        <Step validation={data.name} step={2}>SELECT PLAN</Step>
+        <Step validation={data.plan} step={3}>ADD-ONS</Step>
+        <Step validation={data.plan} step={4}>SUMMARY</Step>
     </section>
   )
 }
