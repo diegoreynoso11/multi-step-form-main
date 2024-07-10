e3b39910daafce6a10e417d3ec83400d09fb1338
apuu03 <57383340+apuu03@users.noreply.github.com>
2024-07-10 16:29:27 -0300

    se agrego "total" y sus tipos

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -7,15 +7,15 @@ interface AppContextProps {
   setError: Dispatch<
     SetStateAction<{ name: string; email: string; phone: string }>
   >
-  data : { name: string; email: string; phone: string; plan: string; addons: string[], billing: string}
-  setData : Dispatch<SetStateAction<{name: string; email: string; phone: string; plan: string; addons: string[], billing : string}>>
+  data : { name: string; email: string; phone: string; plan: string; addons: string[], billing: string, total : number}
+  setData : Dispatch<SetStateAction<{name: string; email: string; phone: string; plan: string; addons: string[], billing : string, total : number}>>
 }
 const defaultValue: AppContextProps = {
   steps: 1,
   setSteps: () => {},
   error: { name: '', email: '', phone: '' },
   setError: () => {},
-  data: { name: '', email: '', phone: '', plan: '', addons: ["","",""], billing: ""},
+  data: { name: '', email: '', phone: '', plan: '', addons: ["","",""], billing: "", total : 0},
   setData: () => {},
 }
 
