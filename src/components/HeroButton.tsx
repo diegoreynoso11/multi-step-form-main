e1bb3aab476f3397d6e4a1d4d23b73f74ee5bd85
apuu03 <57383340+apuu03@users.noreply.github.com>
2024-07-10 18:02:44 -0300

    corregido errores de tipeo

--- src/components/HeroButton.tsx
+++ src/components/HeroButton.tsx
@@ -8,8 +8,8 @@ function HeroButton() {
           <button onClick={() => setSteps(steps - 1)} className="text-blue-800/50 hover:text-blue-800 transition-all px-3 rounded-md font-semibold ring-0 hover:ring-2">
             Go back
           </button>
-        <button onClick={() => setSteps(steps + 1)} className={` bg-[#174a8b] px-3 py-2 hover:bg-[#163052] rounded-md text-white/90`}>
-          {steps === 3 ? "Next step" : "Confirm"}
+        <button onClick={() => setSteps(steps + 1)} className={`bg-[#174a8b] px-3 py-2 hover:bg-[#163052] rounded-md text-white/90`}>
+          {steps === 4 ? "Confirm" : "Next step"}
         </button>
         </div>
   )
